e807c908d7287ac0092f02de8c9b4e80ffde1fc1
oliver <reynoso.orr@gmail.com>
2022-03-04 01:08:50 -0500

    americaweb

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://i.ibb.co/vD5YX9y/peque-o-logo.png" alt="Americatel" />
+                </a>
+            </div>
+            <div className='header--user'>
+                <a href="http://wa.me/51946324747">
+                    <img src="https://i.postimg.cc/282mtmxy/PREUIBALO.png" alt="Usuario" />
+                </a>
+            </div>
+        </header>
+    )
+}
